d2ab5438213184e0df9099da2a30c892cfcbae1e
Brandon May <battleb2014@gmail.com>
2021-08-19 18:43:55 -0400

    almost

---
+++ onboarding/cypress/integration/form_test.js
@@ -0,0 +1,57 @@
+describe('Onboarding App', () => {
+
+    beforeEach(() => {
+        cy.visit( 'http://localhost:3000' )
+    })
+
+    const nameInput = () => cy.get( 'input[ name = name ]' )
+    const emailInput = () => cy.get( 'input[ name = email ]' )
+    const passwordInput = () => cy.get( 'input[ name = password ]' )
+    const foobarInput = () => cy.get('input[name=foobar]')
+    const termsCheckbox = () => cy.get( 'input[ name = terms ]' )
+    const submitBtn = () => cy.get( 'button' )
+
+    it('Make sure tests work', () => {
+        expect(1+1).to.equal(2)
+        expect(5*5).to.equal(25)
+    })
+
+    it('The proper elements are showing', () => {
+        nameInput().should( 'exist' )
+        emailInput().should( 'exist' )
+        passwordInput().should( 'exist' )
+        foobarInput().should( 'not.exist' )
+        termsCheckbox().should( 'exist' )
+        submitBtn().should( 'exist' )
+    })
+
+    describe('Filling in the inputs', () => {
+
+        it('Can navigate to the site', () => {
+            cy.url().should('include', 'localhost')
+        })
+        
+        it('Submit button starts out disabled', () => {
+            submitBtn().should( 'be.disabled' )
+        })
+
+        it('Can type in the inputs', () => {
+            nameInput()
+                .should( 'have.value', '' )
+                .type( 'Carmen' )
+                .should( 'have.value', 'Carmen' )
+
+            emailInput()
+            .should( 'have.value', '' )
+            .type( 'abcdefg@123.com' )
+            .should( 'have.value', 'abcdefg@123.com' )
+
+            passwordInput()
+                .should( 'have.value', '' )
+                .type( '123456789' )
+                .should( 'have.value', '123456789' )
+        })
+        
+    })
+
+})
